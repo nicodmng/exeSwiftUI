38b4b49e51ac330d2cadb81c1a53549d5c4a3eff
NIcolas Demange <nicolas.demange@yahoo.fr>
2023-01-27 08:55:59 +0100

    Initial Commit

---
+++ JaponExe/JaponExeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaponExeApp.swift
+//  JaponExe
+//
+//  Created by Nicolas Demange on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct JaponExeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
